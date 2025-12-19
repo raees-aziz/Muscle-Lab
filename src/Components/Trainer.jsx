645c2530357edcbb65479fa5f5ac7cbd0a467ec3
raees-aziz <raees4963@gmail.com>
2025-12-20 02:04:54 +0500

    changing ui of trainer secion

--- src/Components/Trainer.jsx
+++ src/Components/Trainer.jsx
@@ -3,10 +3,10 @@ import TrainerCard from './mini components/TrainerCard.jsx'
 const exercises = [
     { title: "Jhon Vick", img: "/image/builder-2.jpg" },
     { title: "Adam Zampa", img: "/image/trainer-2.jpg" },
-    { title: "Michael Lucas", img: "/image/trainer-1.jpg" },
-    // { title: "Hypertrophy", img: "/image/exercise4.jpg" },
-    // { title: "Rope Climbing", img: "/image/exercise5.jpg" },
-    // { title: "TRX Suspension", img: "/image/exercise6.jpg" },
+    { title: "Micky", img: "/image/trainer-1.jpg" },
+    { title: "donetello", img: "/image/exercise4.jpg" },
+    { title: "Raphelo", img: "/image/exercise5.jpg" },
+    { title: "Leonardo", img: "/image/exercise6.jpg" },
 ];
 const Trainer = () => {
     return (
@@ -16,13 +16,12 @@ const Trainer = () => {
                     Your Fitness <br /> <span className='text-main'>Goals, Their Expertise </span></h4>
                 <p className='text-gray-300 text-center text-xl'>Our Team Of Certified Trainers Brings Unparalleled Expertise To Help You Acheive Your Fitness Goals</p>
                 <div className="container mx-auto px-4 py-8">
-                    <h2 className="text-3xl font-bold text-center mb-8">Our Exercises</h2>
-                    <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+                    {/* <h2 className="text-3xl font-bold text-center mb-8 text-white">Our Exercises</h2> */}
+                    <div className="grid grid-cols-2 md:grid-cols-6">
                         {exercises.map(({ title, img }, index) => (
-                            <div>
-                                <TrainerCard src={img} name={title} />
-                                {/* <TrainerCard src={img} name={title}/>
-                            <TrainerCard src={img} name={title}/> */}
+                            <div className='' onClick={()=>console.log(title)}>
+                                <img className='border-3 border-main size-40 rounded-full mx-auto object-cover' src={img} alt="" />
+                                <h2 className='text-3xl font-bebas-neue text-white text-center'>{title}</h2>
 
                             </div>
                         ))}
