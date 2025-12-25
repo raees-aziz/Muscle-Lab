f032f1dce626466ea0310be4e4f3d1fdd3111b2d
raees-aziz <raees4963@gmail.com>
2025-12-25 23:59:11 +0500

    bit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -10,9 +10,9 @@ const Contact = () => {
                     <div className='w-full h-20 object-cover absolute -top-10'>
                         <img src="/image/input.png" alt="" />
                     </div>
-                    <input className='absolute right-70 bottom-40 text-center border-primary border-2  text-2xl font-bebas-neue z-100 w-1/2 px-2 py-6 rounded-2xl' type="text" placeholder='abc@musclelab.com' />
+                    <input className='absolute right-70 bottom-40 text-center border-b-2 border-black  text-2xl font-bebas-neue z-100 w-1/2 px-2 py-6 rounded-xl' type="text" placeholder='abc@musclelab.com' />
                     {/* <button className="mx-auto z-100 cursor-none size-20 font-bebas-neue tracking-widest text-xl absolute bottom-10 font-bold rounded-full before:content-['Hello_World'] bg-red-900"></button> */}
-                    <div className='absolute bg-red-600 '><Circular/></div>
+                    <div className='absolute bottom-0 left-75 flex justify-center w-1/2 py-2'><Circular/></div>
                 </div>
 
             </div>
